f6d84177154ac6bc644d8af61e72419d29357304
Ademola08 <164037777+Ademola08@users.noreply.github.com>
2024-07-05 18:41:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,15 +4,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "916909137213"
-global.ownername = "David Cyril"
-global.ytname = "David Cyril"
+global.ownernomer = "2349121056093"
+global.ownername = "tv guy"
+global.ytname = "tv guy"
 global.socialm = "GitHub: DeeCeeXxx"
 global.location = "Africa, Nigeria, Lagos"
 
-global.ownernumber = '2347043759577,2349066528353,254782572110'  //creator number
-global.ownername = 'David Cyril' //owner name
-global.botname = '🕷Venom-2🕷' //name of the bot
+global.ownernumber = '2349121056093'  //creator number
+global.ownername = 'tv guy' //owner name
+global.botname = 'xbug 6' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
@@ -31,7 +31,7 @@ global.prefa = ['','!','.','#','&']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = true //auto typing + recording
+global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
